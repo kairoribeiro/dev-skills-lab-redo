686869ced1025884fbcb5e8752b6040d36b696cd
Kairo Ribeiro <kairo10@gmail.com>
2022-10-04 10:14:52 -0400

    add new and create func

--- controllers/skills.js
+++ controllers/skills.js
@@ -13,6 +13,23 @@ function index(req, res) {
     }) 
 }
 
+function newSkill(req, res) {
+    res.render('skills/new')
+}
+
+function create(req, res) {
+Skill.create(req.body)
+.then(skill => {
+    res.redirect('skills')
+})
+.catch(error => {
+    console.log(error)
+    res.redirect('/')
+}) 
+}
+
 export {
     index,
+    newSkill as new,
+    create
 }
--- routes/skills.js
+++ routes/skills.js
@@ -6,6 +6,12 @@ const router = Router()
 /* GET users listing. */
 router.get('/', skillsCtrl.index)
 
+//GET localhost:3000/skills
+router.get('/new', skillsCtrl.new)
+
+//POST localhost:3000/skills
+router.post('/', skillsCtrl.create)
+
 export {
   router
 }
